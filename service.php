58a87f05ade9aacc77da1ca220f72c679d92d2bc
crazymindstorm <57192134+crazymindstorm@users.noreply.github.com>
2024-05-12 17:14:03 +0200

    Add files via upload
    service website

---
+++ service.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml"><head>
+
+
+
+<!--
+              _                            _
+__      _____| | ___ ___  _ __ ___   ___  | |_ ___
+\ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \
+ \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |
+  \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/
+
+ _________________       ______  _______________________ _______ _______ _ _______       _________________________
+(       )__   __( (    /(  __  \(  ____ \__   __(  ___  |  ____ |       | |  ____ \     (  __)__   __|__   __(__  )
+| () () |  ) (  |  \  ( | (  \  ) (    \/  ) (  | (   ) | (    )| () () |/| (    \/     | (     ) (     ) (     ) |
+| || || |  | |  |   \ | | |   ) | (_____   | |  | |   | | (____)| || || | | (_____ _____| |     | |     | |     | |
+| |(_)| |  | |  | (\ \) | |   | (_____  )  | |  | |   | |     __) |(_)| | (_____  |_____) |     | |     | |     | |
+| |   | |  | |  | | \   | |   ) |     ) |  | |  | |   | | (\ (  | |   | |       ) |     | |     | |     | |     | |
+| )   ( |__) (__| )  \  | (__/  )\____) |  | |  | (___) | ) \ \_| )   ( | /\____) |     | (_____) (___  | |   __) |
+|/     \\_______//    )_|______/\_______)  )_(  (_______)/   \__//     \| \_______)     (____)_______/  )_(  (____)
+
+
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--'
+
+-->
+
+
+
+<!-- Google tag (gtag.js) analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=Ggoogletag"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'Googletag', { 'anonymize_ip':true } );
+</script>
+
+<!-- meta -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0" />
+<meta name="keywords" content="mindstorm's[it], mindstorm's-it, IRC, Chat, 360°, fotos, pictures, live tv, computerdienstleistungen, computer service, Linux, CrazyMindstorm">
+
+
+<title>Service@Mindstorm's[IT]</title>
+<meta name="description" content="Mindstorm's[IT] bietet/serves dir/you Computerdienstleistungen/Computerservice , IRC Chat, 360° Fotos/Pictures, Live TV, so wie einen/and a blog Blog an">
+<meta property="og:image" content="/bilder/icons/android-icon-192x192.png"/>
+<meta name="author" content="Crazy Mindstorm">
+
+<link rel="apple-touch-icon" sizes="57x57" href="/bilder/icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/bilder/icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/bilder/icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/bilder/icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/bilder/icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/bilder/icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/bilder/icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/bilder/icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/bilder/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/bilder/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/bilder/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/bilder/icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/bilder/icons/favicon-16x16.png">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/bilder/icons/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+
+
+
+<!-- css -->
+    <link rel="stylesheet" href="css/mindstormsit.css">
+
+    </head><body>
+
+<div id="page">
+<div id="service">
+<img class="logo" src="https://mindstormsit.net/bilder/mindstormsit-host3.svg" alt="Logo mindstorm's-it.net">
+<!-- test frame kugel -->
+<iframe class="iframer2" id="23" width="200" height="200" src="https://mindstormsit.net/iris.html" aria-label="round iframe showing an iris just for fun / runder iframe der eine iris zeigt (spielerei) " frameborder="1" ></iframe>
+
+<h3 class="colouredText">Willkommen/Welcome💻️: <?php
+    echo htmlspecialchars($_SERVER['REMOTE_ADDR'], ENT_QUOTES, 'UTF-8');
+    ?>
+    your_Host:
+    <?php
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $hostname = gethostbyaddr($ip);
+    $safeHostname = htmlspecialchars($hostname, ENT_QUOTES, 'UTF-8');
+    echo $safeHostname;
+    ?>
+    <br>
+</h3>
+<p>here you can go back to:</p><a target="_blank" href="https://mindstormsit.net" class="ram">Home</a>
+</div>
+<h1 class="glow" >Windows Stuff;</h1>
+<h2>Windows 11 <a target="_blank" href="https://www.microsoft.com/de-de/software-download/windows11">Download</a></h2>
+<h3>Windows upgrader and/und <a target="_blank" href="https://mindstormsit.net/win/keyreader.exe">Keyreader</a>
+on/auf the/der <a target="_blank" href="https://mindstormsit.net/upgrade.php">Upgrade</a> page/seite </h3>
+
+
+<h1 class="glow" >Linux Software;</h1>
+<h3>my/mein favorite/favorisierter audio player audacious <a target="_blank" href="apt://audacious">apt://audacious</a>💡️<a target="_blank" href="https://wiki.ubuntuusers.de/Audacious/">wiki>>ubuntuusers.de;</a> <a target="_blank" href="https://wiki.archlinux.org/index.php/Audacious">wiki>>archlinux.org;</a></h3>
+<h3>my/meine favorite/favorisierte remote Desktop application/Anwendung : supports/unterstützt SSH/VNC/RDP/SPICE <a target="_blank" href="apt://vinagre">apt://vinagre</a>💡️<a target="_blank" href="https://wiki.ubuntuusers.de/Vinagre/">wiki>>ubuntuusers.de;</a></h3>
+<h3>a/eine simple/einfache Python IDE <a target="_blank" href="apt://idle">apt://idle</a></h3>
+<h3>one/einer of/von my/meinen favorite/favorisierten messenger/n <a target="_blank" href="apt://pidgin">apt://pidgin</a>💡️<a target="_blank" href="https://wiki.ubuntuusers.de/Pidgin/">wiki>>ubuntuusers.de;</a> <a target="_blank" href="https://wiki.archlinux.org/title/Pidgin">wiki>>archlinux.org;</a></h3>
+<h3>my/mein second/zweiter favorite/favorisierter XMPP/jabber messenger <a target="_blank" href="apt://gajim">apt://gajim</a>💡️<a target="_blank" href="https://wiki.ubuntuusers.de/Gajim/">wiki>>ubuntuusers.de;</a> <a target="_blank" href="https://wiki.archlinux.org/title/Gajim">wiki>>archlinux.org;</a></h3>
+
+
+<h1 class="glow" >Linux how2's;</h1>
+<h2>Linux Installation:</h2>
+<h3>Eine detailierte Anleitung von <a target="_blank" href="https://learn.microsoft.com/en-us/linux/install">Microsoft;</a>
+<h2>Rechte Verwaltung:</h2>
+<h3><a target="_blank" href="https://wiki.ubuntuusers.de/chmod/">chmod;</a>&<a target="_blank" href="https://wiki.ubuntuusers.de/chown/">chown;</a><< lässt sich auf nahezu jede Linux Distribution übertragen;</h3>
+<h2>fstab:</h2>
+<h3>Mit<a target="_blank" href="https://wiki.ubuntuusers.de/fstab/">fstab</a> Dateisysteme (teil)automatisiert Einhängen;</h3>
+<h2>GParted:</h2>
+<h3>    Festplatten/SSD's partitionieren mit:<a target="_blank" href="https://wiki.ubuntuusers.de/GParted/">GParted (ubuntu-wiki)</a>
+<a target="_blank" href="https://wiki.archlinux.org/title/Parted">GPated (wiki ArchLinux)</a> </h3>
+<h2>Paketverwaltung:</h2>
+<h3>Debian und Derivate (Ubuntu,Kali,LMDE oder Linux Mint usw) nutzen <a target="_blank" href="https://wiki.ubuntuusers.de/APT/">APT</a></h3>
+<h3>Arch und Derivate (Garuda,Manjaro,EndeavourOS oder BlackArch usw) nutzen <a target="_blank" href="https://wiki.manjaro.org/index.php/Pacman_Overview/de">Pacman(Manjao-wiki)</a> <a target="_blank" href="https://wiki.archlinux.de/title/Pacman">Pacman(Arch-wiki)</a></h3>
+<h1 class="wow" >Mixed;</h1>
+<h2>TV Livestream's:</h2> <a href="https://mindstormsit.net/tgs24.php" target="_blank" class="tv" alt="link Tagesschau24">#Tagesschau24</a> <a href="https://mindstormsit.net/swr.php" target="_blank" class="tv" alt="link SWR">#SWR »</a> <a href="https://mindstormsit.net/ARDalpha.php" target="_blank" class="tv" alt="link ARDalpha">#ARD α</a>
+<h2>News:</h2>
+<a href="https://mindstormsit.net/newsaggregator.php" target="_blank" class="ddr4" alt="link to our own newsaggregator with it-news">#IT-News</a>
+<h3>News Channel;<h3>
+<a href="https://t.me/s/Linux_Tech_23" target="_blank" class="ddr4" alt="link to a telegram channel called: Linux_tech_23">Tech❤️Linux🐧️</a>
+
+<h2>Speedtest:<h2>
+<h3>OpenSpeedTest;<h3>
+<a href="https://mindstormsit.net/test/speedtest.php" target="_blank" class="ddr4" alt="link to our own speedtest powered by open speed test">#SpeedTest</a>
+<h1 class="glow" >Partner webs(e)ite/n/s;</h1>
+<h2>FlawCra's Service webs(e)ite:</h2> <a href="https://flawcra.cc/?ref=mindstormsit.net" target="_blank"><img id="logo" src="https://mindstormsit.net/bilder/ddr4-flawcra.webp" alt="link to FlawCra's website"></a>
+<h2>Helden des Bildschirm's Blog, Ki Tools, Service & Game-Sever:</h2> <a href="https://heldendesbildschirms.de/?ref=mindstormsit.net" target="_blank"><img id="logo" src="https://mindstormsit.net/bilder/ddr4-hdb.webp" alt="link to Helden des Bildschirm's website"></a>
+
+<!-- x-perimental javascript wetter -->
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src= "js/weather.js" ></script>
+
+<div class="wetterbox" id="weather">
+</div>
+
+</body></html>
