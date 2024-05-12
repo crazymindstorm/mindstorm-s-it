2a8fb581709226ed709a38305a5139b1c87b44d8
crazymindstorm <57192134+crazymindstorm@users.noreply.github.com>
2024-05-12 17:03:57 +0200

    Add files via upload
    personalized 404 error page

---
+++ 404.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+
+
+
+<!--
+              _                            _
+__      _____| | ___ ___  _ __ ___   ___  | |_ ___
+\ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \
+ \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |
+  \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/
+
+ _________________       ______  _______________________ _______ _______ _ _______       _________________________
+(       )__   __( (    /(  __  \(  ____ \__   __(  ___  |  ____ |       | |  ____ \     (  __)__   __|__   __(__  )
+| () () |  ) (  |  \  ( | (  \  ) (    \/  ) (  | (   ) | (    )| () () |/| (    \/     | (     ) (     ) (     ) |
+| || || |  | |  |   \ | | |   ) | (_____   | |  | |   | | (____)| || || | | (_____ _____| |     | |     | |     | |
+| |(_)| |  | |  | (\ \) | |   | (_____  )  | |  | |   | |     __) |(_)| | (_____  |_____) |     | |     | |     | |
+| |   | |  | |  | | \   | |   ) |     ) |  | |  | |   | | (\ (  | |   | |       ) |     | |     | |     | |     | |
+| )   ( |__) (__| )  \  | (__/  )\____) |  | |  | (___) | ) \ \_| )   ( | /\____) |     | (_____) (___  | |   __) |
+|/     \\_______//    )_|______/\_______)  )_(  (_______)/   \__//     \| \_______)     (____)_______/  )_(  (____)
+
+
+            .,ad88888888baa,
+        ,d8P"""        ""9888ba.
+     .a8"          ,ad88888888888a
+    aP'          ,8888this888888888a
+  ,8"           ,88888888888is8888888,
+ ,8'            (888live88( )888888888,
+,8'             `8888888888let888888888
+8)               `8888it888888888888888,
+8                  "888888peacfully8888)
+8                   `88filled8888888888)
+8)                    "8888with88888888
+(b                     "8888harmony888'
+`8,        (8)          88<388<388888)
+ "8a                   ,8<38<3888888)
+   V8,                 d8<3888<3888"
+    `8b,             ,d88<388<388P'
+      `V8a,       ,ad88<38<3888P'
+         ""88888888<38<38888P"
+              """"""""""""
+
+-->
+
+
+<!-- google analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S7E27WF1GY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'googletag', { 'anonymize_ip':true } );
+</script>
+
+<!-- meta -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0" />
+<meta name="keywords" content="mindstorm's[it], mindstorm's-it, IRC, Chat, 360°, fotos, pictures, live tv, computerdienstleistungen, computer service, Linux, CrazyMindstorm">
+
+
+<title>404@Mindstorm's[IT]</title>
+<meta name="description" content="Mindstorm's[IT] bietet/serves dir/you Computerdienstleistungen/Computerservice , IRC Chat, 360° Fotos/Pictures, Live TV, so wie einen/and a blog Blog an">
+<meta property="og:image" content="/bilder/icons/android-icon-192x192.png"/>
+<meta name="author" content="Crazy Mindstorm">
+
+<link rel="apple-touch-icon" sizes="57x57" href="/bilder/icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/bilder/icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/bilder/icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/bilder/icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/bilder/icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/bilder/icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/bilder/icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/bilder/icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/bilder/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/bilder/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/bilder/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/bilder/icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/bilder/icons/favicon-16x16.png">
+<meta name="msapplication-TileColor" content="#395aed">
+<meta name="msapplication-TileImage" content="/bilder/icons/ms-icon-144x144.png">
+<meta name="theme-color" content="#47a0ed">
+
+
+
+
+<!-- css -->
+    <link rel="stylesheet" href="https://mindstormsit.net/css/mindstormsit.css">
+
+    </head>
+    <body>
+
+<div id="page">
+<div id="header">
+
+<img class="logo" src="https://mindstormsit.net/bilder/mindstormsit-host3.svg">
+
+<h1 style="color: silver;"> </h1>
+    <p>Willkommen/Welcome💻️: <?php
+    echo htmlspecialchars($_SERVER['REMOTE_ADDR'], ENT_QUOTES, 'UTF-8');
+    ?>
+    your_Host:
+    <?php
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $hostname = gethostbyaddr($ip);
+    $safeHostname = htmlspecialchars($hostname, ENT_QUOTES, 'UTF-8');
+    echo $safeHostname;
+    ?>
+    <br>
+</p>
+<p>here you can go back to:</p><a href="https://mindstormsit.net" class="ram">Home</a>
+</div>
+
+<div id="Box2">
+<h1 style="color: silver; font-family: monospace; font-size: 100px;"> error_404 </h1>
+
+</div>
+</body></html>
